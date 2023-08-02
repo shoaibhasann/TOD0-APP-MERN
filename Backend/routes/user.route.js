b4d27d0c3a850b86d6a39b99846e19bdcfc77966
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-03 04:12:08 +0530

    add user routes

---
+++ Backend/routes/user.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { getProfile, login, logout, register } from "../controllers/user.controllers.js";
+import isLoggedIn from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.post('/', register);
+router.post('/login', login);
+router.get('/', isLoggedIn, getProfile);
+router.get('/logout', isLoggedIn, logout);
+
+export default router;
