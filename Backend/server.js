5eef466e51390805d8bdf9435696d2a8421529fb
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-03 04:09:42 +0530

    Add some changes

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,10 @@ import connectDatabase from "./config/db.js";
 dotenv.config(); // load enviroment variables
 
 const port = process.env.PORT || 8001;
+const mode = process.env.NODE_ENV
 
 // Start the server
 app.listen(port, async () => {
   await connectDatabase();
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port} in ${mode} mode`);
 });
