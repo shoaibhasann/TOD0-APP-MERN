63f312baf435c1f835660da049e8e27e4d328194
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-03 04:10:00 +0530

    add cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,19 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import userRoutes from "./routes/user.route.js"
+import taskRoutes from "./routes/task.route.js"
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
 
-app.use('/backend', (req,res) => {
-    res.send('Server running perfectly');
-});
+// Handling user routes
+app.use('/api/v1/user', userRoutes);
+
+// Handling task routes
+app.use('/api/v1/task', taskRoutes);
 
 app.use('*', (req,res) => {
     res.status(404).send("OOPS! Page not found.");
