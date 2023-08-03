3196d132b089b9674c19b599f566a28fcbc6ed16
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-03 07:51:47 +0530

    Add some bugs

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import userRoutes from "./routes/user.route.js"
 import taskRoutes from "./routes/task.route.js"
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cors from "cors";
 
 const app = express();
 
@@ -9,14 +11,25 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+// Middleware for cross origin request
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    credentials: true
+}));
+
 // Handling user routes
 app.use('/api/v1/user', userRoutes);
 
 // Handling task routes
 app.use('/api/v1/task', taskRoutes);
 
+// Handling not defined routes
 app.use('*', (req,res) => {
     res.status(404).send("OOPS! Page not found.");
 });
+
+// Middleware for error handling
+app.use(errorMiddleware);
 
 export default app;
